498390fbfb060e11c69a91149cb94f44a286a54e
Edgar Eldy <ed.eldy21@live.com>
2021-08-02 23:54:39 +0200

    Setting up headers

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,12 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+// Setting up headers
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
